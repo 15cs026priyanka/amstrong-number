247d74cd62a020d7aa28ea1749a91e163531566a
15cs026priyanka <priyankaappu1998@gmail.com>
2018-02-23 14:13:15 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,10 @@
+lower =100
+upper=200
+lower=int(input("enter a lower range")):
+upper=int(input("enter a upper range")):
+for num in range (lower,upper)
+order(length(sum))
+sum=0
+temp=num
+while>0
+print(num)
